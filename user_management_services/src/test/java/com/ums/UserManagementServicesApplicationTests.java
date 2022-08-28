ba7e9e68deb785fab3214a2fabe8457f82a8395a
rajan navagare <rajan.v.navagare@gmail.com>
2022-08-28 21:30:00 +0530

    new project Usermanagement service

---
+++ user_management_services/src/test/java/com/ums/UserManagementServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
